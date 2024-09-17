deb9c738b5c7bac8283bdd55e8bb239b2f4edd2a
Jasper Sands <jaspersands@wustl.edu>
2024-09-17 18:57:27 -0500

    begin

---
+++ 437/437/_37App.swift
@@ -0,0 +1,17 @@
+//
+//  _37App.swift
+//  437
+//
+//  Created by Jasper Sands on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct _37App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
