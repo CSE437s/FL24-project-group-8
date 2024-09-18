5082eccf13744a39e5fa5470fbea5d607f5f7b65
ryandickersonnn <dickerson.r@wustl.edu>
2024-09-17 19:19:30 -0500

    changing xcode files

---
+++ swiftFrontEnd/swiftFrontEnd/swiftFrontEndApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftFrontEndApp.swift
+//  swiftFrontEnd
+//
+//  Created by Ryan Dickerson on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftFrontEndApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
