4d8a6658cf149e92acf805c7c148b8a5eaa6f57a
Jasper Sands <jasper@mindtrip.ai>
2024-10-01 16:58:47 -0500

    quest video working

--- swiftFrontEnd/swiftFrontEnd/Quest1ViewController.swift
+++ swiftFrontEnd/swiftFrontEnd/Quest1ViewController.swift
@@ -5,15 +5,55 @@
 //  Created by Mohammed Ali on 9/24/24.
 //
 
+import AVKit
 import UIKit
 
-class Quest1ViewController: UIViewController {
+class Quest1ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func recordVideoButtonTapped(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let imagePickerController = UIImagePickerController()
+            imagePickerController.delegate = self
+            imagePickerController.sourceType = .camera
+            imagePickerController.mediaTypes = ["public.movie"]
+            imagePickerController.videoQuality = .typeHigh
+            imagePickerController.cameraCaptureMode = .video
+            
+            self.present(imagePickerController, animated: true, completion: nil)
+        } else {
+            // Alert user that the camera is not available
+            let alert = UIAlertController(title: "Error", message: "Camera is not available.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alert, animated: true, completion: nil)
+        }
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let videoURL = info[.mediaURL] as? URL {
+            // Dismiss the picker
+            picker.dismiss(animated: true) {
+                // Play the recorded video
+                let player = AVPlayer(url: videoURL)
+                let playerViewController = AVPlayerViewController()
+                playerViewController.player = player
+                
+                self.present(playerViewController, animated: true) {
+                    player.play()
+                }
+            }
+        }
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+
     
 
     /*
